4048bef01b55021a361168aefde8bdfa6a430ed3
hwq <weiqi.huang@pisofttech.com>
2018-05-29 12:18:37 +0800

    更新imodel

--- basic/src/main/java/com/abt/basic/arch/mvvm/model/IModel.java
+++ basic/src/main/java/com/abt/basic/arch/mvvm/model/IModel.java
@@ -1,11 +1,13 @@
 package com.abt.basic.arch.mvvm.model;
 
+import com.abt.basic.arch.mvvm.view.load.BaseLoadListener;
+
 /**
  * @描述： @IModel
  * @作者： @黄卫旗
  * @创建时间： @2018/5/29
  */
-public interface IModel {
+public interface IModel<T> {
 
-    void getData();
+    void getData(BaseLoadListener<T> listener);
 }
